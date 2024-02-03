6b2b8581096951a592a09c577851f96c8719b979
Abhishek Sharma <mrhashcoder@Abhisheks-MacBook-Pro.local>
2024-02-03 18:34:42 +0530

    INIT Using Template

---
+++ app/content/page.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function page() {
+  return <div>Content Page</div>
+}
+
+export default page
---
+++ lib/links.ts
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    route: "Home",
+    path: "/",
+  },
+  {
+    route: "Create",
+    path: "/create",
+  },
+  {
+    route: "Generated Content",
+    path: "/content",
+  },
+]
